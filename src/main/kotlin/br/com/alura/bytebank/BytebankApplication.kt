64deaa691a5624da04a3dec353ca05d0e5744fda
Alex Felipe <alexfelipevieira@gmail.com>
2019-10-11 14:27:20 -0300

    atualizando código para features padrões

--- src/main/kotlin/br/com/alura/bytebank/BytebankApplication.kt
+++ src/main/kotlin/br/com/alura/bytebank/BytebankApplication.kt
@@ -1,11 +1,30 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.model.Feature
+import br.com.alura.bytebank.service.FeatureService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class BytebankApplication
 
 fun main(args: Array<String>) {
 	runApplication<BytebankApplication>(*args)
+}
+
+private const val featureHistorico = "historico"
+
+private const val featureTransferir = "transferir"
+
+@Component
+class SalvaFeatures(private val service: FeatureService) {
+
+	@PostConstruct
+	fun init(){
+		service.salva(Feature(featureTransferir))
+		service.salva(Feature(featureHistorico))
+	}
+
 }
--- src/main/kotlin/br/com/alura/bytebank/model/Feature.kt
+++ src/main/kotlin/br/com/alura/bytebank/model/Feature.kt
@@ -1,13 +1,11 @@
 package br.com.alura.bytebank.model
 
-import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity
 class Feature(
         @Id
-        val id: String = UUID.randomUUID().toString(),
         val nome: String,
         val disponivel: Boolean = true
 )
